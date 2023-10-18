89fb63f6cf7ace06de3143d2baf427f5747867f4
DarkMelodias <samueltriana159@gmail.com>
2023-10-17 22:40:44 -0500

    Cambios backend hechos

--- piproyecto/api/procedures.py
+++ piproyecto/api/procedures.py
@@ -1,7 +1,7 @@
 from django.db import connection
 
-def my_custom_sql(proposito, marca, sis_operativo, cap_disco):
+def my_custom_sql(proposito, marca, sis_operativo, cap_disco,tip_com):
     with connection.cursor() as cursor:
-        cursor.callproc('obtenerEquipos', [proposito, marca, sis_operativo, cap_disco])
-        result = cursor.fetchone()
+        cursor.callproc('obtenerEquipos', [proposito, marca, sis_operativo, cap_disco,tip_com])
+        result = cursor.fetchall()
     return result
